54934c42ded6463f363fb7a5a1100cec0b1c3802
Lorenzo Mion <lorenzodfmion@gmail.com>
2022-03-06 10:04:27 -0300

    join and leave working; done button without functionality

--- components/WaitingRoom.js
+++ components/WaitingRoom.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
 
-import { collection, query, where, getDocs, addDoc} from "firebase/firestore";
+import { collection, query, where, getDocs, addDoc, onSnapshot, deleteDoc, setDoc, doc} from "firebase/firestore";
 
 export function WaitingRoom(props) {
 
@@ -12,38 +12,58 @@ export function WaitingRoom(props) {
 
     const [joined, setJoined] = useState(false)
 
+    const fencersRef = collection(props.eventRef, 'fencers')
+
     useEffect(async () => {
         console.log('is on use effect')
-        const fencersRef = collection(props.eventRef, 'fencers')
-        const fencersDocs  = getDocs(fencersRef)
-        const fencers = [];
-        (await fencersDocs).forEach(doc => fencers.push(doc.data()))
-        console.log(fencers)
-        setFencers(fencers)
+        /*
+        const fencersDocs = await getDocs(fencersRef)*/
+        const unsubscribe = onSnapshot(fencersRef, (snapshot) => {
+            const fencers = [];
+            snapshot.forEach(doc => fencers.push(doc.data()))
+            console.log(fencers)
+            setFencers(fencers)
+        })
     }, [])
 
     const join = async () => {
-        const fencerRef = await addDoc(collection(props.eventRef, 'fencers'), {
+        const fencerRef = await setDoc(doc(props.eventRef, 'fencers', props.user.uid), {
             userName: props.user.displayName,
             id: props.user.uid
         })
     }
 
+    const leave = async () => {
+        await deleteDoc(doc(fencersRef, props.user.uid))
+    }
+
+    if(!fencers){return null}
+
     return (<>
         <div className='mainContent'>
             <h3>Waiting Room</h3>
             {
-                fencers ? 
+                fencers.length!==0 ? 
                 <ul className='card'>
-                    {fencers.map(fencer => <li><p>{fencer.userName}</p></li>)}
+                    {fencers.map(fencer => <li>
+                        <p>{fencer.userName}</p>
+                        {
+                            fencer.id === props.user.uid ? 
+                            <button className='button button-secondary' onClick={leave}>Leave</button> :
+                            <button className='button button-terciary'>Remove</button>
+                        }
+                    </li>)}
                 </ul> :
                 <p>Join or add fencers</p>
             }
         </div>
-        {!joined && 
-            <div className='button-container'>
-                <button className='button button-primary' onClick={join}>Join</button>
-            </div>
-        }
+        <div className='button-container'>
+            {
+                (!fencers.some((fencer) => fencer.id === props.user.uid)) ?
+                <button className='button button-primary' onClick={join}>Join</button>  :
+                <button className='button button-primary' >Done</button> 
+            }
+        </div>
+        
     </>)
 }
