4a9fe38a14f206144922c3292119ba3a076da6f7
Lorenzo Mion <lorenzodfmion@gmail.com>
2022-06-22 20:11:38 -0300

    finished starter page

---
+++ pages/event/[event]/starter/index.jsx
@@ -0,0 +1,78 @@
+import React, {useState, useEffect} from 'react'
+import { useRouter } from "next/router";
+import NavBar from '../../../../components/NavBar';
+import { db } from "../../../../firebase/firebase.js";
+import { doc, onSnapshot, collection } from "firebase/firestore";
+
+const Starter = () => {
+
+    const router = useRouter();
+	const { event } = router.query;
+
+	const [eventData, setEventData] = useState(undefined);
+	const [fencers, setFencers] = useState(undefined);
+
+    class Fencer {
+		id;
+        startingRank;
+		//pool;
+		userName;
+		constructor({ id, userName, startingRank }) {
+			this.id = id;
+			this.userName = userName;
+            this.startingRank = startingRank;
+		}
+	}
+
+    useEffect(async () => {
+		if (!router.isReady) return;
+		const eventRef = doc(db, "Events", event);
+
+		const getEvent = onSnapshot(eventRef, doc => {
+			//console.log("Current data: ", doc.data());
+			setEventData(doc.data());
+		});
+
+		const fencersRef = collection(eventRef, "fencers");
+
+		const getFencers = onSnapshot(fencersRef, querySnapshot => {
+			const fencersData = [];
+			querySnapshot.forEach(doc => {
+				const id = doc.id;
+				console.log(doc.data());
+				console.log(id);
+				const fencerObject = new Fencer({ ...doc.data(), id });
+				console.log(fencerObject);
+				fencersData.push(fencerObject);
+			});
+			console.log("fromDatabase", fencersData);
+			setFencers(fencersData);
+		});
+
+		//setEventRef(eventRef);
+	}, [router.isReady]);
+
+    if(!eventData || !fencers){
+        return null
+    }
+
+    return (<>
+        <NavBar
+				tabs={true}
+				eventName={eventData.name}
+                eventId={event}
+				currentTab="starter"
+			/>
+            <div className="mainContent">
+                <h3>Pool Results</h3>
+                <ol className='card column'>
+                        {fencers.sort((fencerA, fencerB) => fencerA.startingRank - fencerB.startingRank).map((fencer, index) => <li key={index} className="participant-in-list">
+                            <p>{fencer.userName}</p>
+                            <p>{index+1}</p>
+                        </li>)}
+                </ol>
+        </div>
+    </>)
+}
+
+export default Starter;
