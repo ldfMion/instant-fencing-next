eea6d23be855c23ddc14996ee39f5c6d226f6b3c
Lorenzo Mion <lorenzodfmion@gmail.com>
2022-07-12 12:18:52 +0200

    fixed sorttype and sortByRank permissions

--- components/SelectSortType.js
+++ components/SelectSortType.js
@@ -1,48 +1,74 @@
 import React, { useState } from "react";
 
-import {setDoc} from "firebase/firestore";
+import { setDoc } from "firebase/firestore";
 
 export function SelectSortType(props) {
-    const [value, setValue] = useState("");
-    const handleChange = (event) => {
-        setValue(event.target.value);
-    }
-    const handleSubmit = async (event) => {
-        console.log('is submitting')
-        event.preventDefault();
-        await setDoc(props.eventRef, {
-            sortType: value
-        }, {merge: true});
-    }
-    console.log(value)
-    console.log(props.eventRef)
-    return (
-            <form  onChange={handleChange} onSubmit={handleSubmit}>
-                <div className='mainContent'>
-                    <h3>Select Competition Sort Type</h3>
-                    <ul className='card column'>
-                        <li className="participant-in-list">
-                            <label htmlFor="Randomly">Randomly</label>
-                            <input
-                                type="radio"
-                                id="Randomly"
-                                name="sort"
-                                value="Randomly"
-                            />
-
-                        </li>
-                        <li className="participant-in-list">
-                            <label htmlFor="By Rank">By Rank</label>
-                            <input type="radio" id="By Rank" name="sort" value="By Rank" />
-                        </li>
-                    </ul>
-                    {value === '' && <p>Choose how the fencers will be sorted into pools.</p>}
-                    {value === 'Randomly' && <p>Fencers will be sorted randomly into pools.</p>}
-                    {value === 'By Rank' && <p>Select Fencers from best to worst. This rank will be used to arrage them in pools.</p>}
-                </div>
-                <div className='button-container'>
-                    <button className='button-primary' type='submit' disabled={value === ""}>Confirm</button>
-                </div>
-            </form>
-    );
+	const [value, setValue] = useState("");
+	const handleChange = event => {
+		setValue(event.target.value);
+	};
+	const handleSubmit = async event => {
+		console.log("is submitting");
+		event.preventDefault();
+		await setDoc(
+			props.eventRef,
+			{
+				sortType: value,
+			},
+			{ merge: true }
+		);
+	};
+	console.log(value);
+	console.log(props.eventRef);
+	return (
+		<form onChange={handleChange} onSubmit={handleSubmit}>
+			<div className="mainContent">
+				<h3>Select Competition Sort Type</h3>
+				<ul className="card column">
+					<li className="participant-in-list">
+						<label htmlFor="Randomly">Randomly</label>
+						<input
+							type="radio"
+							id="Randomly"
+							name="sort"
+							value="Randomly"
+						/>
+					</li>
+					<li className="participant-in-list">
+						<label htmlFor="By Rank">By Rank</label>
+						<input
+							type="radio"
+							id="By Rank"
+							name="sort"
+							value="By Rank"
+						/>
+					</li>
+				</ul>
+                {!props.user && <p>Log in to continue creating event</p>}
+				{value === "" && (
+					<p>Choose how the fencers will be sorted into pools.</p>
+				)}
+				{value === "Randomly" && (
+					<p>Fencers will be sorted randomly into pools.</p>
+				)}
+				{value === "By Rank" && (
+					<p>
+						Select Fencers from best to worst. This rank will be
+						used to arrage them in pools.
+					</p>
+				)}
+			</div>
+			{props.user && (
+				<div className="button-container">
+					<button
+						className="button-primary"
+						type="submit"
+						disabled={value === ""}
+					>
+						Confirm
+					</button>
+				</div>
+			)}
+		</form>
+	);
 }
--- components/SortByRank.js
+++ components/SortByRank.js
@@ -1,68 +1,123 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-import { collection, onSnapshot, setDoc, doc} from "firebase/firestore";
+import { collection, onSnapshot, setDoc, doc } from "firebase/firestore";
 
 export function SortByRank(props) {
-    const [unselectedFencers, setUnselectedFencers] = useState([])
-    const [selectedFencers, setSelectedFencers] = useState([])
-    const fencersRef = collection(props.eventRef, 'fencers')
-    useEffect(async () => {
-        console.log('is on use effect')
-        const getFencers = onSnapshot(fencersRef, (snapshot) => {
-            const fencers = [];
-            snapshot.forEach(doc => {
-                const id = doc.id;
-                fencers.push({...doc.data(), id})
-            });
-            console.log(fencers)
-            setUnselectedFencers(fencers)
-        })
-    }, [])
+	const [unselectedFencers, setUnselectedFencers] = useState([]);
+	const [selectedFencers, setSelectedFencers] = useState([]);
+	const fencersRef = collection(props.eventRef, "fencers");
+	useEffect(async () => {
+		console.log("is on use effect");
+		const getFencers = onSnapshot(fencersRef, snapshot => {
+			const fencers = [];
+			snapshot.forEach(doc => {
+				const id = doc.id;
+				fencers.push({ ...doc.data(), id });
+			});
+			console.log(fencers);
+			setUnselectedFencers(fencers);
+		});
+	}, []);
 
-    const addFencer = async(fencerToAdd) => {
-        setSelectedFencers([...selectedFencers, fencerToAdd])
-        setUnselectedFencers(unselectedFencers.filter(fencer => fencer !== fencerToAdd))
-    }
-    const removeFencer = async(fencerToRemove) => {
-        setUnselectedFencers([...unselectedFencers, fencerToRemove]);
-        setSelectedFencers(selectedFencers.filter(fencer => fencer !== fencerToRemove))
-    }
-    const handleSubmit = async (e) => {
-        console.log('submitting')
-        e.preventDefault()
-        await setDoc(props.eventRef, {
-            fencersAreSorted: true
-        }, {merge: true});
-        selectedFencers.forEach(async(fencer, index) => {
-            await setDoc((doc(fencersRef, fencer.id)), {
-                startingRank: index+1
-            }, {merge: true})
-        })
-    }
-    console.log(selectedFencers)
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <div className='mainContent'>
-                    <h3>Sort By Rank</h3>
-                    <p>Select Fencers from best to worst. This rank will be used to arrage them in pools.</p>
-                    <ol className='card column'>
-                        {selectedFencers.map((fencer, index) => <li key={index} className="participant-in-list">
-                            <p>{index+1}</p>
-                            <label htmlFor={fencer.userName}>{fencer.userName}</label>
-                            <input type="checkbox" id={fencer.userName} checked={true} name={fencer.userName} value={fencer.userName} onChange={() => removeFencer(fencer)}/>
-                        </li>)}
-                    </ol>
-                    <ul className='card column'>
-                        {unselectedFencers.map((fencer, index) => <li key={index} className="participant-in-list">
-                            <label htmlFor={fencer.userName}>{fencer.userName}</label>
-                            <input type="checkbox" id={fencer.userName} name={fencer.userName} value={fencer.userName} checked={false} onChange={() => addFencer(fencer)}/>
-                        </li>)}
-                    </ul>
-                </div>
-                <div className='button-container'>
-                    <button className='button-primary' type='submit' disabled={unselectedFencers.length !== 0}>Confirm</button>
-                </div>
-            </form>
-        </>)
-    }
+	const addFencer = async fencerToAdd => {
+		setSelectedFencers([...selectedFencers, fencerToAdd]);
+		setUnselectedFencers(
+			unselectedFencers.filter(fencer => fencer !== fencerToAdd)
+		);
+	};
+	const removeFencer = async fencerToRemove => {
+		setUnselectedFencers([...unselectedFencers, fencerToRemove]);
+		setSelectedFencers(
+			selectedFencers.filter(fencer => fencer !== fencerToRemove)
+		);
+	};
+	const handleSubmit = async e => {
+		console.log("submitting");
+		e.preventDefault();
+		await setDoc(
+			props.eventRef,
+			{
+				fencersAreSorted: true,
+			},
+			{ merge: true }
+		);
+		selectedFencers.forEach(async (fencer, index) => {
+			await setDoc(
+				doc(fencersRef, fencer.id),
+				{
+					startingRank: index + 1,
+				},
+				{ merge: true }
+			);
+		});
+	};
+	console.log(selectedFencers);
+	return (
+		<>
+			<form onSubmit={handleSubmit}>
+				<div className="mainContent">
+					<h3>Sort By Rank</h3>
+					{props.user && (
+						<p>
+							Select Fencers from best to worst. This rank will be
+							used to arrage them in pools.
+						</p>
+					)}
+					{!props.user && (
+						<p className="fail-text">Log in to continue creating the event</p>
+					)}
+					<ol className="card column">
+						{selectedFencers.map((fencer, index) => (
+							<li key={index} className="participant-in-list">
+								<p>{index + 1}</p>
+								<label htmlFor={fencer.userName}>
+									{fencer.userName}
+								</label>
+								{props.user && (
+									<input
+										type="checkbox"
+										id={fencer.userName}
+										checked={true}
+										name={fencer.userName}
+										value={fencer.userName}
+										onChange={() => removeFencer(fencer)}
+									/>
+								)}
+							</li>
+						))}
+					</ol>
+					<ul className="card column">
+						{unselectedFencers.map((fencer, index) => (
+							<li key={index} className="participant-in-list">
+								<label htmlFor={fencer.userName}>
+									{fencer.userName}
+								</label>
+								{props.user && (
+									<input
+										type="checkbox"
+										id={fencer.userName}
+										name={fencer.userName}
+										value={fencer.userName}
+										checked={false}
+										onChange={() => addFencer(fencer)}
+									/>
+								)}
+							</li>
+						))}
+					</ul>
+				</div>
+				{props.user && (
+					<div className="button-container">
+						<button
+							className="button-primary"
+							type="submit"
+							disabled={unselectedFencers.length !== 0}
+						>
+							Confirm
+						</button>
+					</div>
+				)}
+			</form>
+		</>
+	);
+}
