451b08fa246428a6453c7f94503c06780abed60a
Lorenzo Mion <lorenzodfmion@gmail.com>
2022-11-22 16:11:35 -0300

    commented out logged in to edit bouts poss

--- components/BoutSide.jsx
+++ components/BoutSide.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 import styles from "../styles/BoutSide.module.css";
 
+//import {auth} from '../firebase/firebase.js'
+//import { useAuthState } from 'react-firebase-hooks/auth';
+
 export const BoutSide = ({
 	fencer,
 	fencerNumber,
 	fencerScore,
 	updateScore,
 }) => {
+
+    //const [user] = useAuthState(auth);
+
 	return (
 		<div className={styles.boutSide}>
 			<p>{fencerNumber}</p>
 			<p className="fill-container">{fencer.userName}</p>
-			<input
-				type="number"
-				className={styles.scoreInput}
-				value={fencerScore}
+            <input
+                type="number"
+                className={styles.scoreInput}
+                value={fencerScore}
                 max={5}
                 min={0}
-				onChange={e => {
+                onChange={e => {
                     const score = e.target.value;
                     if(score > 5 || score < 0) return
-					updateScore(parseInt(score));
-				}}
-			></input>
+                    updateScore(parseInt(score));
+                }}
+                //disabled={!user}
+            ></input>
 		</div>
 	);
 };
