925b8d0bece06e1afaa91cc3cc457caebb406cdf
Lorenzo Mion <lorenzodfmion@gmail.com>
2023-05-15 19:29:41 -0400

    added SSR to starter eventData

--- pages/event/[event]/starter/index.jsx
+++ pages/event/[event]/starter/index.jsx
@@ -6,6 +6,7 @@ import {
 	doc,
 	onSnapshot,
 	collection,
+    getDoc
 } from "firebase/firestore";
 import Head from "next/head";
 
@@ -13,7 +14,7 @@ const useGetEventAndFencersData = (routerIsReady, event) => {
 	console.log("is on function");
 	console.log("router is ready", routerIsReady);
 
-	const [eventData, setEventData] = useState(undefined);
+	//const [eventData, setEventData] = useState(undefined);
 	const [fencers, setFencers] = useState(undefined);
 
 	useEffect(async () => {
@@ -34,10 +35,12 @@ const useGetEventAndFencersData = (routerIsReady, event) => {
 		setEventData(eventData);
         */
 
+        /*
         const getEventData = onSnapshot(eventRef, docSnapshot => {
             const eventData = docSnapshot.data()
             setEventData(eventData)
         })
+        */
 
         /*
 		const fencersRef = collection(eventRef, "fencers");
@@ -68,26 +71,26 @@ const useGetEventAndFencersData = (routerIsReady, event) => {
 		console.log("got data");
 	}, [event]);
 	console.log("returning data");
-	return { eventData: eventData, fencers: fencers };
+	return {fencers: fencers };
 };
 
-const Starter = () => {
+const Starter = ({eventData}) => {
 	const router = useRouter();
 	const { event } = router.query;
 	const routerIsReady = router.isReady;
 
-	const { eventData, fencers } = useGetEventAndFencersData(
+	const { fencers } = useGetEventAndFencersData(
 		routerIsReady,
 		event
 	);
 	console.log(eventData, fencers);
 
-	const dataIsLoaded = eventData && fencers;
+	const dataIsLoaded = !!fencers;
 
 	return (
 		<>
 			<Head>
-				<title>{dataIsLoaded && eventData.name}: starter</title>
+				<title>{eventData.name}: starter</title>
 				<meta
 					name="description"
 					content="Automate the creation of fencing competitions during practice."
@@ -107,8 +110,8 @@ const Starter = () => {
 			</Head>
 			<NavBar
 				tabs={true}
-				eventName={dataIsLoaded && eventData.name}
-				eventId={event}
+				eventName={eventData.name}
+				eventId={eventData.name}
 				currentTab="starter"
 			/>
 			{dataIsLoaded && (
@@ -134,6 +137,19 @@ const Starter = () => {
 };
 
 export default Starter;
+
+export async function getServerSideProps({params}) {
+    // Fetch data from external API
+    console.log(params.id)
+    const eventRef = doc(db, "Events", params.event);
+
+    const eventData = (await getDoc(eventRef)).data()
+    delete eventData.createdAt
+  
+    // Pass data to the page via props
+    console.log(eventData)
+    return { props: { eventData } }
+  }
 
 class Fencer {
 	id;
