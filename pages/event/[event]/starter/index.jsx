132f618cbe40d7196c26cc6dd1a37b0b6492bc5e
Lorenzo Mion <lorenzodfmion@gmail.com>
2023-05-15 19:14:41 -0400

    data fetching custom hook outside component

--- pages/event/[event]/starter/index.jsx
+++ pages/event/[event]/starter/index.jsx
@@ -1,85 +1,148 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import NavBar from '../../../../components/NavBar';
+import NavBar from "../../../../components/NavBar";
 import { db } from "../../../../firebase/firebase.js";
-import { doc, onSnapshot, collection, getDoc, getDocs } from "firebase/firestore";
+import {
+	doc,
+	onSnapshot,
+	collection,
+} from "firebase/firestore";
 import Head from "next/head";
 
-const Starter = () => {
-
-    const router = useRouter();
-	const { event } = router.query;
+const useGetEventAndFencersData = (routerIsReady, event) => {
+	console.log("is on function");
+	console.log("router is ready", routerIsReady);
 
 	const [eventData, setEventData] = useState(undefined);
 	const [fencers, setFencers] = useState(undefined);
 
-    useEffect(() => {
-		if (!router.isReady) return;
+	useEffect(async () => {
+		console.log("is on use effect");
 
-        async function getPageData(){
-            const eventRef = doc(db, "Events", event);
-    
-            const eventData = await getDoc(eventRef);
-            setEventData(eventData.data());
-    
-            const fencersRef = collection(eventRef, "fencers");
-    
-            const fencersSnap = await getDocs(fencersRef);
+		if (!routerIsReady) {
+			console.log("router is not ready");
+			return;
+		}
+
+		console.log("is getting page data");
+		const eventRef = doc(db, "Events", event);
+
+        /*
+		const eventResponse = await getDoc(eventRef);
+		const eventData = eventResponse.data();
+		console.log(eventData);
+		setEventData(eventData);
+        */
+
+        const getEventData = onSnapshot(eventRef, docSnapshot => {
+            const eventData = docSnapshot.data()
+            setEventData(eventData)
+        })
+
+        /*
+		const fencersRef = collection(eventRef, "fencers");
+
+		const fencersSnap = await getDocs(fencersRef);
+		const fencersData = [];
+		fencersSnap.forEach(fencerSnap => {
+			const id = fencerSnap.id;
+			fencersData.push(new Fencer({ ...fencerSnap.data(), id }));
+		});
+		setFencers(fencersData);
+        */
+
+		const fencersRef = collection(eventRef, "fencers");
+
+        const getFencersData = onSnapshot(fencersRef, fencersSnapshot => {
+            console.log("is getting fencers")
             const fencersData = [];
-            fencersSnap.forEach(fencerSnap => {
-                const id = fencerSnap.id;
-                fencersData.push(new Fencer({...fencerSnap.data(), id}));
-    
-            })
-            setFencers(fencersData)
-        }
-        getPageData();
+            fencersSnapshot.forEach(doc => {
+                const id = doc.id;
+                fencersData.push(new Fencer({ ...doc.data(), id }));
+            });
+            console.log(fencers)
+            setFencers(fencersData);
+        })
 
-	}, [router.isReady, event]);
+		console.log("seetting fencers");
+		console.log("got data");
+	}, [event]);
+	console.log("returning data");
+	return { eventData: eventData, fencers: fencers };
+};
 
-    if(!eventData || !fencers){
-        return null
-    }
+const Starter = () => {
+	const router = useRouter();
+	const { event } = router.query;
+	const routerIsReady = router.isReady;
 
-    return (<>
-        <Head>
-            <title>{eventData.name}: starter</title>
-            <meta name="description" content="Automate the creation of fencing competitions during practice."/>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <meta name="robots" content="index, follow"/>
-            <meta charset="UTF-8"/>
-            <meta property='og:title' content="Instant Fencing Beta Test"/>
-            <meta property="og:description" content="Automate the creation of fencing competitions during practice." />
-            <meta property="og:type" content="website" />
-        </Head>
-        <NavBar
+	const { eventData, fencers } = useGetEventAndFencersData(
+		routerIsReady,
+		event
+	);
+	console.log(eventData, fencers);
+
+	const dataIsLoaded = eventData && fencers;
+
+	return (
+		<>
+			<Head>
+				<title>{dataIsLoaded && eventData.name}: starter</title>
+				<meta
+					name="description"
+					content="Automate the creation of fencing competitions during practice."
+				/>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+				<meta name="robots" content="index, follow" />
+				<meta charset="UTF-8" />
+				<meta property="og:title" content="Instant Fencing Beta Test" />
+				<meta
+					property="og:description"
+					content="Automate the creation of fencing competitions during practice."
+				/>
+				<meta property="og:type" content="website" />
+			</Head>
+			<NavBar
 				tabs={true}
-				eventName={eventData.name}
-                eventId={event}
+				eventName={dataIsLoaded && eventData.name}
+				eventId={event}
 				currentTab="starter"
 			/>
-            <div className="mainContent">
-                <h3>Starter</h3>
-                <ol className='card column'>
-                        {fencers.sort((fencerA, fencerB) => fencerA.startingRank - fencerB.startingRank).map((fencer, index) => <li key={index} className="participant-in-list">
-                            <p>{fencer.userName}</p>
-                            <p>{index+1}</p>
-                        </li>)}
-                </ol>
-        </div>
-    </>)
-}
+			{dataIsLoaded && (
+				<div className="mainContent">
+					<h3>Starter</h3>
+					<ol className="card column">
+						{fencers
+							.sort(
+								(fencerA, fencerB) =>
+									fencerA.startingRank - fencerB.startingRank
+							)
+							.map((fencer, index) => (
+								<li key={index} className="participant-in-list">
+									<p>{fencer.userName}</p>
+									<p>{index + 1}</p>
+								</li>
+							))}
+					</ol>
+				</div>
+			)}
+		</>
+	);
+};
 
 export default Starter;
 
 class Fencer {
-    id;
-    startingRank;
-    //pool;
-    userName;
-    constructor({ id, userName, startingRank }) {
-        this.id = id;
-        this.userName = userName;
-        this.startingRank = startingRank;
-    }
+	id;
+	startingRank;
+	//pool;
+	userName;
+	constructor({ id, userName, startingRank }) {
+		this.id = id;
+		this.userName = userName;
+		this.startingRank = startingRank;
+	}
 }
