c6b9d79990fe75e8c4fef01e8a3a3a273906b4a9
Lorenzo Mion <lorenzodfmion@gmail.com>
2023-06-09 22:28:48 -0300

    copied the meta title to the social media sharing

--- pages/event/[event]/pools/index.jsx
+++ pages/event/[event]/pools/index.jsx
@@ -5,45 +5,10 @@ import Head from "next/head";
 import NavBar from "../../../../components/NavBar";
 import {PoolPreview} from '../../../../components/PoolPreview'
 
-import { db } from "../../../../firebase/firebase.js";
-import { doc, onSnapshot, collection, orderBy, query } from "firebase/firestore";
-
 import useGetPools from "../../../../data/useGetPools";
 import getServerSideEventData from "../../../../data/getServerSideEventData";
 
 export default function Pools({serverSideEventData}) {
-    //const router = useRouter();
-    //const {event} = router.query
-    //const [eventRef, setEventRef] = useState(undefined)
-    //const [eventData, setEventData] = useState(undefined)
-    //const [pools, setPools] = useState([])
-    const eventRef = doc(db, "Events", serverSideEventData.id);
-    /*
-    useEffect(()=>{
-        if(!router.isReady) return;        
-        const eventRef = doc(db, "Events", event);
-        //setEventRef(eventRef)
-
-        const getEvent = onSnapshot(eventRef, (doc) => {
-            console.log("Current data: ", doc.data());
-            setEventData(doc.data())
-        });
-
-        const poolsRef = query(collection(eventRef,'pools'), orderBy("poolId", "asc"))
-
-        const getPools = onSnapshot(poolsRef, (querySnapshot) => {
-            const pools = [];
-            querySnapshot.forEach((doc) => {
-                const id = doc.id
-                pools.push({...doc.data(), id});
-            });
-            setPools(pools);
-        });
-
-        setEventRef(eventRef);
-
-    }, [router.isReady, event]);
-    */
 
     const pools = useGetPools(serverSideEventData.id)
 
@@ -52,12 +17,12 @@ export default function Pools({serverSideEventData}) {
     return (
         <>
             <Head>
-                <title>{serverSideEventData.name}: pools</title>
+                <title>{serverSideEventData.name}: pools - Instant Fencing</title>
                 <meta name="description" content="Automate the creation of fencing competitions during practice."/>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta name="robots" content="index, follow"/>
                 <meta charset="UTF-8"/>
-                <meta property='og:title' content="Instant Fencing Beta Test"/>
+                <meta property='og:title' content={`${serverSideEventData.name}: pools - Instant Fencing`}/>
                 <meta property="og:description" content="Automate the creation of fencing competitions during practice." />
                 <meta property="og:type" content="website" />
             </Head>
@@ -78,44 +43,6 @@ export default function Pools({serverSideEventData}) {
             </div>
         </>
     )
-    /*
-    console.log('is on pools')
-    const router = useRouter();
-    const { event } = router.query;
-    console.log(router)
-    const eventRef =  doc(db, "Events", event)
-    const poolsRef = collection(eventRef,'pools')
-    const [eventData, setEventData] = useState(undefined);
-    const [pools, setPools] = useState([])
-
-    useEffect(async () => {
-        if (!router.isReady) return;
-        //const eventRef = doc(db, "Events", event);
-        //setEventRef(eventRef);
-
-        const getEvent = onSnapshot(eventRef, (doc) => {
-            console.log("Current data: ", doc.data());
-            setEventData(doc.data());
-        });
-
-        //const getFencers = onSnapshot()
-
-        const getPools = onSnapshot(poolsRef, (querySnapshot) => {
-            const pools = [];
-            querySnapshot.forEach((doc) => {
-                const id = doc.id
-                pools.push({...doc.data(), id});
-            });
-            setPools(pools);
-          });
-        
-    }, [router.isReady]);
-
-    if (!eventData) {
-        return null;
-    }
-
-    ;*/
 }
 
 export async function getServerSideProps({ params }) {
