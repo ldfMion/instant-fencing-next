580ce7f3ad2c0f017767ac1f33f8c7663bcbd0d8
Lorenzo Mion <lorenzodfmion@gmail.com>
2024-07-31 22:18:32 -0300

    streamlined log in with google button

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React from "react";
+import styles from "../styles/NavLinks.module.css";
+import Link from "next/link";
+import LoginButton from "./LoginButton";
 
-import styles from '../styles/NavLinks.module.css';
-
-import {SignOut} from './SignOut.jsx'
-
-import Link from 'next/link'
-
-export const NavLinks = (props) => {
-    return (
-        <menu className={styles.NavLinks + ' ' + (props.open ? styles.NavLinks_open : styles.NavLinks_closed)}>
-            <Link href='/home' passHref><button className='button-terciary'><a>Home</a></button></Link>
-            <Link href='/' passHref><button className='button-terciary'><a>About</a></button></Link>
-            <SignOut/>
-        </menu>
-    );
-}
-
+export const NavLinks = props => {
+	return (
+		<menu
+			className={
+				styles.NavLinks +
+				" " +
+				(props.open ? styles.NavLinks_open : styles.NavLinks_closed)
+			}
+		>
+			<Link href="/home" passHref>
+				<button className="button-terciary">
+					<a>Home</a>
+				</button>
+			</Link>
+			<Link href="/" passHref>
+				<button className="button-terciary">
+					<a>About</a>
+				</button>
+			</Link>
+			<LoginButton showSignOut={true} />
+		</menu>
+	);
+};
--- pages/home.jsx
+++ pages/home.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
+import React from "react";
 
 /*
 import { getAuth } from "firebase/auth";
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 */
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState } from "react-firebase-hooks/auth";
 
-import {HomePage} from '../components/HomePage';
-import {LogPrompt} from '../components/LogPrompt';
+import { HomePage } from "../components/HomePage";
+import { LoggedOut } from "../components/LoggedOut.jsx";
 
-import {auth, db} from '../firebase/firebase.js'
+import { auth, db } from "../firebase/firebase.js";
 
 const Home = () => {
-    console.log('is rendering the coponent again')
-    //get the user that is logged in
-    console.log(db)
-    //const auth = getAuth();
-    const [user] = useAuthState(auth);
-    console.log(user)
-    
-    //load the prompt to sign in/up if the user is not signed in
-    
-    if(!user){
-        return (<LogPrompt/>)
-    }
+	console.log("is rendering the coponent again");
+	//get the user that is logged in
+	console.log(db);
+	//const auth = getAuth();
+	const [user] = useAuthState(auth);
+	console.log(user);
 
-    return (
-        <>
-            <HomePage user={user} db={db}/>
-        </>
-    )
-    
-}
+	//load the prompt to sign in/up if the user is not signed in
+
+	if (!user) {
+		return <LoggedOut />;
+	}
+
+	return (
+		<>
+			<HomePage user={user} db={db} />
+		</>
+	);
+};
 
 export default Home;
